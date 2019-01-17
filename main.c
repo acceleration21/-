23dbe7b731f09a7a392f2d3fc74380492acef5da
wangjunze <171635094@qq.com>
2019-01-17 08:53:56 +0800

    添加敏感字符

--- main.c
+++ main.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
-int index;
-int arr[1024]
+int index = 0;
+int arr[1024];
 int main()
 {
+    
+    while(1){
+        
+    
+    
+   
 
-    printf("1--添加敏感字符");
-    printf("2--删除最后一个敏感字符");
-    printf("3--显示所有敏感字符");
-    printf("4--替换敏感字符");
-    printf("5--退出");
+    printf("1--添加敏感字符\n");
+    printf("2--删除最后一个敏感字符\n");
+    printf("3--显示所有敏感字符\n");
+    printf("4--替换敏感字符\n");
+    printf("5--退出\n");
 
-     printf("请输入相应的功能编号:");
+     printf("请输入相应的功能编号:\n");
      int code;
      scanf("%d",&code);
      
      if (code == 1) {
-         
+         printf("输入要添加的字符:\n");
+         char m;
+         scanf("%c",&m);
+         scanf("%c",&m);
+         arr[index] = m;
+         index++;
+
+         printf("添加成功，回车继续\n");
+         char x;
+         scanf("%c",&x);
+         scanf("%c",&x);
          
      }
       if (code == 2) {
@@ -31,7 +47,7 @@ int main()
          /* code */
      }
      
-
+    }
 
     return 0;
 }
