2695abd0534697d8943dc61e3018d10c6043f324
wangjunze <171635094@qq.com>
2019-01-17 08:43:08 +0800

    基本框架

--- main.c
+++ main.c
@@ -1,15 +1,37 @@
 #include <stdio.h>
-
+int index;
+int arr[1024]
 int main()
 {
 
     printf("1--添加敏感字符");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
+    printf("2--删除最后一个敏感字符");
+    printf("3--显示所有敏感字符");
+    printf("4--替换敏感字符");
+    printf("5--退出");
 
-     printf("");
+     printf("请输入相应的功能编号:");
+     int code;
+     scanf("%d",&code);
+     
+     if (code == 1) {
+         
+         
+     }
+      if (code == 2) {
+         /* code */
+     }
+      if (code == 3) {
+         /* code */
+     }
+      if (code == 4) {
+         /* code */
+     }
+      if (code == 5) {
+         /* code */
+     }
+     
+
 
     return 0;
 }
